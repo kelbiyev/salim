23fd85355c660bbb89211e9895c3132bb700eeae
Salim <kelbiyev11@mail.ru>
2024-09-17 10:03:30 +0400

    Add files via upload

---
+++ main3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+enum class Color{
+	white,
+	red,
+	blue,
+	green
+};
+
+int main (){
+	/*
+	Color color1 = Color::red;
+	Color color2 = Color::green;
+	
+	int value = static_cast<int>(color2);
+	std::cout<<value<<"\n";
+	Color color3 = static_cast<Color>(2);
+	*/
+	 
+	
+	
+	return 0;
+}
+	
+
+	
